e3189d8609ff1f7935904ac28841e18680af748a
rajeshsunnam358 <rajeshsunnam358@gmail.com>
2022-05-12 22:59:58 +0530

    24

--- ReverseNumberExample1.java
+++ ReverseNumberExample1.java
@@ -2,7 +2,7 @@ public class ReverseNumberExample1
 {  
 public static void main(String[] args)   
 {  
-int number = a987654, reverse = 0;  
+int number = 12987654, reverse = 0;  
 while(number != 0)   
 {  
 int remainder = number % 10;  
