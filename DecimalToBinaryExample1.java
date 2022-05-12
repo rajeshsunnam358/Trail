1f7951b7358257e547f17651f252800972ccb141
rajeshsunnam358 <rajeshsunnam358@gmail.com>
2022-05-12 22:48:22 +0530

    22

--- DecimalToBinaryExample1.java
+++ DecimalToBinaryExample1.java
@@ -1,6 +1,6 @@
 public class DecimalToBinaryExample1{  
 public static void main(String args[]){  
-System.out.println(Integer.toBinaryString(10));  
-System.out.println(Integer.toBinaryString(21));  
-System.out.println(Integer.toBinaryString(31));  
+System.out.println(Integer.toBinaryString(1024));  
+System.out.println(Integer.toBinaryString(251));  
+System.out.println(Integer.toBinaryString(3167));  
 }}  
