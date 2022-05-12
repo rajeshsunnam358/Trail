126c42bfbfd761673535f9c6ffe813422a6107e7
rajeshsunnam358 <rajeshsunnam358@gmail.com>
2022-05-12 22:46:38 +0530

    19

--- Armstrong.java
+++ Armstrong.java
@@ -2,7 +2,7 @@ public class Armstrong {
 
     public static void main(String[] args) {
 
-        int number = 370, originalNumber, remainder, result = 0;
+        int number = 375, originalNumber, remainder, result = 0;
 
         originalNumber = number;
 
