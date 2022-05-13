44a7954b97138baf546bc110d827c15c5e2c31ad
rajeshsunnam358 <rajeshsunnam358@gmail.com>
2022-05-13 17:03:13 +0530

    r6

--- FactorialExample.java
+++ FactorialExample.java
@@ -1,7 +1,7 @@
 class FactorialExample{  
  public static void main(String args[]){  
   int i,fact=1;  
-  int number=8;//It is the number to calculate factorial    
+  int number=12;//It is the number to calculate factorial    
   for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
