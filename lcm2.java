f468b34f797fce447982c0f2b6fb31e32766f461
rajeshsunnam358 <rajeshsunnam358@gmail.com>
2022-05-19 17:34:26 +0530

    457

--- lcm2.java
+++ lcm2.java
@@ -1,7 +1,7 @@
 public class Lcm {
   public static void main(String[] args) {
 
-    int n1 = 76, n2 =190, lcm;
+    int n1 = 75, n2 =125, lcm;
 
     // maximum number between n1 and n2 is stored in lcm
     lcm = (n1 > n2) ? n1 : n2;
